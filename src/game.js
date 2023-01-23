796ce55a3f0146934042bfc985aa363c6ada5368
Rafael Gilberto Rezende Lima <rgrl516@gmail.com>
2023-01-23 10:09:31 -0300

    arquivo inicial

---
+++ src/game.js
@@ -0,0 +1,14 @@
+class Player {
+    constructor(name, health, strengh) {
+        this.name = name;
+        this.health = health;
+        this.strengh = strengh;
+        this.cards = [];
+    }
+    receiveDamage(damage){
+        this.health -= damage;
+    }
+    attack() {
+        return this.strengh;
+    }
+}

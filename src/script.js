3fe63cd76574a270d5de4839c9e408cf1152cfd9
Rafael Gilberto Rezende Lima <rgrl516@gmail.com>
2023-01-24 13:08:37 -0300

    first two pages and the board of the game

--- src/script.js
+++ src/script.js
@@ -1,10 +1,40 @@
 const btnNext = document.getElementById("btn-next");
 const btnStart = document.getElementById("btn-start");
-const x = document.getElementById("selection");
+const slc = document.getElementById("firstPart");
+const gameBoard = document.getElementById("gameSection");
+const btnReturn = document.getElementById("btn-back");
+const elementHierarchyImg = document.getElementById("elements-hierarchy");
 
-btnNext.addEventListener("click",()=>{
-    header.classList.add("hidden");
+btnNext.addEventListener("click", () => {
+  header.classList.add("hidden");
+  slc.classList.replace("hidden", "selection");
 });
-btnStart.addEventListener("click",()=>{
-    x.classList.add("hidden");
+
+btnReturn.addEventListener("click", () => {
+  slc.classList.replace("selection", "hidden");
+  header.classList.remove("hidden");
 });
+
+btnStart.addEventListener("click", () => {
+  slc.classList.replace("selection", "hidden");
+  gameBoard.classList.replace("hidden", "gameSection");
+  elementHierarchyImg.classList.remove("hidden");
+});
+
+function gameResult(player, bot) {
+  if (player == "earth" && bot == "water") {
+    return "bot";
+  } else if (player == "earth" && bot == "fire") {
+    return "player";
+  } else if (player == "water" && bot == "earth") {
+    return "player";
+  } else if (player == "water" && bot == "fire") {
+    return "bot";
+  } else if (player == "fire" && bot == "earth") {
+    return "bot";
+  } else if (player == "fire" && bot == "water") {
+    return "player";
+  } else {
+    return "empate";
+  }
+}
